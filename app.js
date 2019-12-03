1aa6fc0eb7069e0ba450daabca3972ac954e2523
dolpshing <ryuki.parker@gmail.com>
2019-12-02 23:10:57 -0800

    add and delete buttons are now functional
    add button can add the value in the textbox to the list (including empty spaces for now)
    delete button will delete every item in the list

--- app.js
+++ app.js
@@ -2,19 +2,42 @@ const textBoxText = document.getElementById('textbox');
 const addButton = document.getElementById('add-btn');
 const clearButton = document.getElementById('clear-btn');
 const deleteAllButton = document.getElementById('delete-all-btn');
+const itemList = document.getElementById('item-list');
 
 function main() {
-  addButton.addEventListener("click", () => {
-    console.log(`add button clicked and added ${textBoxText.value}`);
-  })
-  
-  clearButton.addEventListener("click", () => {
-    console.log("clear button clicked");
-  })
-  
-  deleteAllButton.addEventListener("click", () => {
-    console.log("delete all button clicked");
-  })
+    addButton.addEventListener("click", () => {
+        console.log(`${textBoxText.value} has been added to the list`);
+        addToList(textBoxText.value);
+    })
+
+    clearButton.addEventListener("click", () => {
+        console.log("clear button clicked");
+    })
+
+    deleteAllButton.addEventListener("click", () => {
+        console.log("all list items have been deleted");
+        deleteAllItems();
+    })
 }
 
 main();
+
+function addToList(item) {
+    const newListItem = document.createElement('li');
+    newListItem.innerHTML = item;
+    itemList.appendChild(newListItem);
+}
+
+function deleteAllItems() {
+    // sets first to the first child element of the parent "itemList"
+    let first = itemList.firstElementChild;
+    // while loop as long as there remains a "firstElementChild"
+    while (first) {
+        // remove the child
+        first.remove();
+        // as soon as the child is removed, assign first to the next first (second one, etc.)
+        first = itemList.firstElementChild;
+        // eventually there will be no more children, and the loop will stop
+        // all items will be deleted
+    }
+}
