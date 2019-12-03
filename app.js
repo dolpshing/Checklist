9e863a7f46d08b3f318444faf16a71e1beb6a870
ryukiparker <ryuki.parker@gmail.com>
2019-12-02 21:37:32 -0800

    added list container and responsive buttons

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//this is not empty
+const textBoxText = document.getElementById('textbox');
+const addButton = document.getElementById('add-btn');
+const clearButton = document.getElementById('clear-btn');
+const deleteAllButton = document.getElementById('delete-all-btn');
+
+function main() {
+  addButton.addEventListener("click", () => {
+    console.log(`add button clicked and added ${textBoxText.value}`);
+  })
+  
+  clearButton.addEventListener("click", () => {
+    console.log("clear button clicked");
+  })
+  
+  deleteAllButton.addEventListener("click", () => {
+    console.log("delete all button clicked");
+  })
+}
+
+main();
