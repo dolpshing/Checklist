98f6ed6bc563f3bafdae1f3db9e8a6d853d84e86
dolpshing <ryuki.parker@gmail.com>
2019-12-04 15:13:07 -0800

    Edited few methods, enter key to add to list
    Enter key can now add to list
    Adding or clearing an item to the list will clear the text box of any text
    No longer able to add empty text to the list

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+// Grab HTML elements
 const textBoxText = document.getElementById('textbox');
 const addButton = document.getElementById('add-btn');
 const clearButton = document.getElementById('clear-btn');
 const deleteAllButton = document.getElementById('delete-all-btn');
 const itemList = document.getElementById('item-list');
 
+// Main execution
 function main() {
+    
+    checkButtonPress();
+    enterKeyPress();
+}
+
+main();
+
+// Checks for clicks on any of the 3 buttons
+function checkButtonPress() {
     addButton.addEventListener("click", () => {
         console.log(`${textBoxText.value} has been added to the list`);
         addToList(textBoxText.value);
@@ -20,15 +31,32 @@ function main() {
     })
 }
 
-main();
-
-function addToList(item) {
-    const newListItem = document.createElement('li');
-    newListItem.innerHTML = item;
-    itemList.appendChild(newListItem);
+// Checks for if the enter key is pressed and exectues addToList()
+function enterKeyPress() {
+  textBoxText.addEventListener("keypress", (e) => {
+    if (e.which === 13 || e.keyCode === 13 || e.key === "Enter") {
+      console.log("enter key pressed");
+      addToList(textBoxText.value);
+      return false;
+    }
+  })
 }
 
+// Creates a new list item and inserts the text in
+function addToList(item) {
+    if (textBoxText.value != "") {
+      textBoxText.value = "";
+      const newListItem = document.createElement('li');
+      newListItem.innerHTML = item;
+      itemList.appendChild(newListItem);
+    } else {
+      null;
+    }
+}
+
+// Runs a loop that deletes every list item in itemList
 function deleteAllItems() {
+    textBoxText.value = "";
     // sets first to the first child element of the parent "itemList"
     let first = itemList.firstElementChild;
     // while loop as long as there remains a "firstElementChild"
