2900ea6fced427a80c3c286d946b4f075567deda
dolpshing <ryuki.parker@gmail.com>
2019-12-04 17:32:35 -0800

    THE CHECKBOXES WORK
    I FRIED MY BRAIN FOR 2 HOURS BUT IT FINALLY WORKED!!!!!!!

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const addButton = document.getElementById('add-btn');
 const clearButton = document.getElementById('clear-btn');
 const deleteAllButton = document.getElementById('delete-all-btn');
 const itemList = document.getElementById('item-list');
+const checkbox = document.getElementsByClassName('checkbox');
 
 // Main execution
 function main() {
-    
+
     checkButtonPress();
     enterKeyPress();
 }
@@ -33,30 +34,56 @@ function checkButtonPress() {
 
 // Checks for if the enter key is pressed and exectues addToList()
 function enterKeyPress() {
-  textBoxText.addEventListener("keypress", (e) => {
-    if (e.which === 13 || e.keyCode === 13 || e.key === "Enter") {
-      console.log("enter key pressed");
-      addToList(textBoxText.value);
-      return false;
-    }
-  })
+    textBoxText.addEventListener("keypress", (e) => {
+        if (e.which === 13 || e.keyCode === 13 || e.key === "Enter") {
+            console.log("enter key pressed");
+            addToList(textBoxText.value);
+            return false;
+        }
+    })
 }
 
 // Creates a new list item and inserts the text in
 function addToList(item) {
     if (textBoxText.value != "") {
-      textBoxText.value = "";
-      const newListItem = document.createElement('li');
-      newListItem.innerHTML = item;
-      itemList.appendChild(newListItem);
+        textBoxText.value = "";
+        const newListItem = document.createElement('li');
+        const listItemText = document.createElement('p');
+        const listItemCheckbox = document.createElement('div');
+
+        listItemText.innerHTML = item;
+        listItemCheckbox.classList.add('checkbox');
+        listItemCheckbox.classList.add('unchecked');
+        newListItem.appendChild(listItemText);
+        newListItem.appendChild(listItemCheckbox);
+        itemList.appendChild(newListItem);
+
+        listItemCheckbox.addEventListener('click', () => {
+            console.log("checkbox clicked");
+            if (listItemCheckbox.classList.contains('unchecked')) {
+                listItemCheckbox.classList.remove('unchecked')
+                listItemCheckbox.classList.add('checked');
+            } else if (listItemCheckbox.classList.contains('checked')) {
+                listItemCheckbox.classList.remove('checked');
+                listItemCheckbox.classList.add('unchecked');
+            }
+        })
     } else {
-      null;
+        null;
+    }
+}
+
+function clearSelected() {
+    const selected = document.getElementsByClassName('checked');
+    let first = selected.firstElementChild;
+    while (first) {
+        first.remove();
+        first = selected.firstElementChild;
     }
 }
 
 // Runs a loop that deletes every list item in itemList
 function deleteAllItems() {
-    textBoxText.value = "";
     // sets first to the first child element of the parent "itemList"
     let first = itemList.firstElementChild;
     // while loop as long as there remains a "firstElementChild"
